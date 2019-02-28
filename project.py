d2e94af7719e70edb151e3a42bc9c13cc1307e4b
tigran-kocharyan <tigran.kocharyan@outlook.com>
2019-03-01 00:14:29 +0500

    main

---
+++ project.py
@@ -0,0 +1,34 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import requests
+import datetime
+
+up = Updater("728506589:AAEwkNES9a9koAm8CKaOqUDorarnRJaeFY4")
+dp = up.dispatcher
+
+def start(bot, up):
+    up.message.reply_text('Hello (*・ω・)ﾉ')
+    bot.sendDocument(chat_id=up.message.chat_id, document='CAADAQAD4AEAAkWQ0AeCTzUa7LnRbQI')
+
+def echo(bot, up):
+    up.message.reply_text("Undefined ┐('～`;)┌")
+
+def weather(bot, up):
+    api='0f798fa08e77c5b4a2ad9d1bcbf5d700'
+    try:
+        settings = {'q': 'Tashkent', 'units': 'metric', 'lang': 'en', 'APPID': api}
+        r = requests.get('http://api.openweathermap.org/data/2.5/weather', params=settings)
+        data=r.json()
+        bot.sendMessage(chat_id=up.message.chat_id, text=f"Weather in Tashkent: {int(data['main']['temp_max'])}°C")
+    except Exception as e:
+        print("Exception (weather):", e)
+        pass
+
+dp = up.dispatcher
+start = CommandHandler("start", start)
+weather = CommandHandler("weather", weather)
+dp.add_handler(start)
+dp.add_handler(weather)
+dp.add_handler(MessageHandler(Filters.text, echo))
+
+up.start_polling()
+up.idle()
