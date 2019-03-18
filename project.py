dddfe9254c0b731c76f2c1cdf07ea2fe1327bed1
Tigran Kocharyan <tigran.kocharyan@outlook.com>
2019-03-18 23:32:01 +0500

    Update project.py

--- project.py
+++ project.py
@@ -67,16 +67,17 @@ def button_check(bot, up):
     elif up.message.reply_to_message.text == "Enter the number of years and months since your last birthday (use strictly this order with a space between them):": 
         years_months=up.message.text.split() #splitting the answer into separated words
         db_add((int(years_months[0]))*12+int(years_months[1]), 'age', up.message.chat.id)
-        bot.sendMessage(up.message.chat.id, "Your age is added. Check the table!👌", reply_markup = remove)
-            
+        bot.sendMessage(up.message.chat.id, "Your age is added. Check the table!👌", reply_markup = remove)         
             
     elif up.message.reply_to_message.text == "Enter your weight (use point with floating point numbers):":
-        db_add(float(up.message.text), 'weight', up.message.chat.id)
-        bot.sendMessage(up.message.chat.id, "Your weight is added. Check the table!👌", reply_markup = remove)
+        if (float(up.message.text)>0):
+            db_add(float(up.message.text), 'weight', up.message.chat.id)
+            bot.sendMessage(up.message.chat.id, "Your weight is added. Check the table!👌", reply_markup = remove)
             
-    elif up.message.reply_to_message.text == "Enter your height in integers:" and int(up.message.text)>0:
-        db_add(int(up.message.text), 'height', up.message.chat.id)
-        bot.sendMessage(up.message.chat.id, "Your height is added. Check the table!👌", reply_markup = remove)
+    elif up.message.reply_to_message.text == "Enter your height in integers:":
+        if (int(up.message.text)>0):
+            db_add(int(up.message.text), 'height', up.message.chat.id)
+            bot.sendMessage(up.message.chat.id, "Your height is added. Check the table!👌", reply_markup = remove)
         
     else:
         bot.sendMessage(chat_id=up.message.chat.id, text="Ooops, sorry, incorrect data. Try again! ┐('～`;)┌", reply_markup=remove)
