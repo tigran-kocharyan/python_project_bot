0d2953286404c7a42010406f3594f711bb5a9071
tigran-kocharyan <tigran.kocharyan@outlook.com>
2019-03-31 00:28:11 +0500

    Jobs

--- project.py
+++ project.py
@@ -7,69 +7,76 @@ import psycopg2 as sql
 import os
 
 up = Updater("728506589:AAEwkNES9a9koAm8CKaOqUDorarnRJaeFY4")
+j = up.job_queue
 dp = up.dispatcher
 bd = sql.connect(host="ec2-54-247-118-238.eu-west-1.compute.amazonaws.com", dbname="d94l6q6a6g2mvm", user="jguojvyjehytsn", password="2fa3407deca1f2bc920fefc973912b3ed4388f727c6bd88039552d6032a458d9")
 cur=bd.cursor()
 
+#___________________Commands Settings____________________________________________________#
 def start(bot, up):
     up.message.reply_text('Hello (*・ω・)ﾉ')
     bot.sendDocument(chat_id=up.message.chat_id, document='CAADAQAD4AEAAkWQ0AeCTzUa7LnRbQI')
     check_id(up.message.chat.id)
 
 def echo(bot, up):
-    exit_admin(bot, up)
     button_check(bot, up)
 
 #___________________ID + BD Settings_____________________________________________________#
-
-def check_id(id):                                                   # Пополнение базы data
+def check_id(id):                                                   # Занесение ID в базу данных data
     cur.execute("SELECT id from data;")
     data_id = cur.fetchall()
     if (id,) not in data_id:
         cur.execute("INSERT into data (id) VALUES({0});".format(id))
         bd.commit()
 
-def db_add(number, param, id_db):
+def db_add(number, param, id_db):                                   # Занесение параметров пользователя в data
     cur.execute(f"UPDATE data SET {param} = {number} WHERE id = {id_db};")
     bd.commit()
 
-#___________________Panel Settings_______________________________________________________#
+#___________________Panel + Buttons Settings_____________________________________________#
 
-keyb=[["Weight","Height", "Age"],["EXIT"]]
-bot_panel=telegram.ReplyKeyboardMarkup(keyb,resize_keyboard=True,one_time_keyboard=True)
 remove=telegram.ReplyKeyboardRemove()
 force=telegram.ForceReply()
 
+med_keyb=[["Weight","Height", "Age"],["EXIT"]]                      # Med Settings
+med_panel_set=telegram.ReplyKeyboardMarkup(med_keyb,resize_keyboard=True,one_time_keyboard=True)
+
+reminder_keyb=[["Remind"],["EXIT"]]                                 # Remind Settings
+reminder_panel_set=telegram.ReplyKeyboardMarkup(reminder_keyb,resize_keyboard=True,one_time_keyboard=True)
+
+def buttons():
+    keys = [[InlineKeyboardButton('Tomorrow', callback_data='1'), InlineKeyboardButton('No, thanks!', callback_data='2')]]
+    return InlineKeyboardMarkup(inline_keyboard=keys)
+
 def med_panel(bot, up):
-    bot.sendMessage(up.message.chat.id,"Choose:",reply_markup=bot_panel)
+    bot.sendMessage(up.message.chat.id,"Choose:",reply_markup=med_panel_set)
+
+def reminder_panel(bot, up):
+    bot.sendMessage(up.message.chat.id,"What to remind in a minute?:",reply_markup=reminder_panel_set)
 
 #___________________Panel Processing_____________________________________________________#
-
-def exit_admin(bot,up):
-    if up.message.text=="EXIT": 
-        bot.sendMessage(up.message.chat.id,"Exit (ノωヽ)",reply_markup=remove)
-
 def button_check(bot, up):
     try:
-    #___________________Weight Processing________________________________________________#
-        if up.message.text=="Weight":
+        if up.message.text=="EXIT": 
+            bot.sendMessage(up.message.chat.id,"Exit (ノωヽ)",reply_markup=remove)
+        elif up.message.text=="Remind":
+            bot.sendMessage(chat_id=up.message.chat.id, text="I will remind you something in a minute")
+            j.run_once(callback_minute, 60)
+
+        elif up.message.text=="Weight": # Weight Processing
             bot.sendMessage(chat_id=up.message.chat.id, text="Enter your weight (use point with floating point numbers):", reply_markup=force)
-    #___________________Height Processing________________________________________________#
-        elif up.message.text=="Height":
+        elif up.message.text=="Height": # Height Processing
             bot.sendMessage(chat_id=up.message.chat.id, text="Enter your height in integers:", reply_markup=force)
-    #___________________Age Processing___________________________________________________#
-        elif up.message.text=="Age":
+        elif up.message.text=="Age":    # Age Processing
             bot.sendMessage(chat_id=up.message.chat.id, text="Enter the number of years and months since your last birthday (use strictly this order with a space between them):", reply_markup=force)
-    #___________________Answer Processing________________________________________________#
-        elif up.message.reply_to_message.text == "Enter the number of years and months since your last birthday (use strictly this order with a space between them):": 
+                                        
+        elif up.message.reply_to_message.text == "Enter the number of years and months since your last birthday (use strictly this order with a space between them):": # Answer Processing
             years_months=up.message.text.split() #splitting the answer into separated words
             db_add((int(years_months[0]))*12+int(years_months[1]), 'age', up.message.chat.id)
-            bot.sendMessage(up.message.chat.id, "Your age is added. Check the table!👌", reply_markup = remove)  
-               
+            bot.sendMessage(up.message.chat.id, "Your age is added. Check the table!👌", reply_markup = remove)                 
         elif up.message.reply_to_message.text == "Enter your weight (use point with floating point numbers):" and (float(up.message.text))>=0:
             db_add(float(up.message.text), 'weight', up.message.chat.id)
-            bot.sendMessage(up.message.chat.id, "Your weight is added. Check the table!👌", reply_markup = remove)
-       
+            bot.sendMessage(up.message.chat.id, "Your weight is added. Check the table!👌", reply_markup = remove)      
         elif up.message.reply_to_message.text == "Enter your height in integers:" and (int(up.message.text))>0:
             db_add(int(up.message.text), 'height', up.message.chat.id)
             bot.sendMessage(up.message.chat.id, "Your height is added. Check the table!👌", reply_markup = remove) 
@@ -78,8 +85,7 @@ def button_check(bot, up):
     except:
         bot.sendMessage(chat_id=up.message.chat.id, text="Ooops, sorry, incorrect data. Try again! ┐('～`;)┌", reply_markup=remove)
 
-#___________________Buttons Settings_____________________________________________________#
-
+#___________________Buttons Processing___________________________________________________#
 def get_callback_from_button(bot, up):
     query = up.callback_query
     chat_id = query.message.chat.id
@@ -98,14 +104,11 @@ def get_callback_from_button(bot, up):
         bot.sendMessage(chat_id=chat_id, text="You're welcome (^_~)")
         query.answer()
 
-def buttons():
-    keys = [[InlineKeyboardButton('Tomorrow', callback_data='1'), InlineKeyboardButton('No, thanks!', callback_data='2')]]
-    return InlineKeyboardMarkup(inline_keyboard=keys)
-
-
+#___________________Reminding Processing_________________________________________________#
+def callback_minute(context):
+    context.bot.send_message(chat_id=context.up.message.chat.id, text='Reminding!👍')
 
 #___________________Tashkent Weather_____________________________________________________#
-
 def weather(bot, up):
     api = '0f798fa08e77c5b4a2ad9d1bcbf5d700'
     try:
@@ -118,15 +121,11 @@ def weather(bot, up):
         print("Exception (weather):", e)
         pass
 
-#___________________dispatcher settgings_________________________________________________#
+#___________________Dispatcher settgings_________________________________________________#
 
-dp = up.dispatcher
-start = CommandHandler("start", start)
-med_panel = CommandHandler("med_panel", med_panel)
-weather = CommandHandler("weather", weather)
-dp.add_handler(start)
-dp.add_handler(med_panel)
-dp.add_handler(weather)
+dp.add_handler(CommandHandler("start", start))
+dp.add_handler(CommandHandler("med_panel", med_panel))
+dp.add_handler(CommandHandler("weather", weather))
 dp.add_handler(MessageHandler(Filters.text, echo))
 dp.add_handler(CallbackQueryHandler(get_callback_from_button))
 
